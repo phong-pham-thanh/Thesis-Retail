de9284e3f4aec0375fd81342f4d615e8d481e870
Tuan Nguyen <qtngmin@gmail.com>
2024-09-24 04:55:38 +0700

    [Add] category page and popup edit category information

--- Front-End/react/src/app/api_links.ts
+++ Front-End/react/src/app/api_links.ts
@@ -1,105 +1,118 @@
-
 const authUrl = "https://localhost:7030";
 
 const api_links = {
-    product: {
-        // GET ALL PRODUCTS
-        getAll: {
-            url: `${authUrl}/Product`,
-            method: "GET",
-        },
-
-        // GET PRODUCT BY ID
-        getById: (productId: number) => ({
-            url: `${authUrl}/Product/getProductById/${productId}`,
-            method: "GET",
-        }),
-
-        // CREATE NEW PRODUCT
-        createNew: {
-            url: `${authUrl}/Product/addNewProduct`,
-            method: "POST",
-            data: {},
-        },
-
-        // EDIT PRODUCT
-        edit: (productId: number) => ({
-            url: `${authUrl}/Product/updateProductById/${productId}`,
-            method: "PUT",
-            data: {}, // This will be populated with the product data to update
-        }),
-
-        // DELETE PRODUCT
-        delete: (productId: number) => ({
-            url: `${authUrl}/Product/deleteProductById/${productId}`,
-            method: "DELETE",
-        }),
+  product: {
+    // GET ALL PRODUCTS
+    getAll: {
+      url: `${authUrl}/Product`,
+      method: "GET",
     },
 
-    goodsIssue: {
-        //GET ALL
-        getAll: {
-            url: `${authUrl}/GoodRecipt/GetAllGoodRecipts`,
-            method: "GET",
-        },
-        //GET BY ID
-        getById: {
-            url: `${authUrl}/GoodRecipt`,
-            method: "GET",
-        },
+    // GET PRODUCT BY ID
+    getById: (productId: number) => ({
+      url: `${authUrl}/Product/getProductById/${productId}`,
+      method: "GET",
+    }),
 
-        //POST
-        createNew: {
-            url: `${authUrl}/GoodRecipt/addGoodRecipt`,
-            method: "POST",
-            data: {},
-        },
-
-        ////////////////////// ELSE //////////////////////
-        edit: {
-            url: `${authUrl}api/Product`,
-            method: "PUT",
-            token: "",
-            data: {}
-        },
+    // CREATE NEW PRODUCT
+    createNew: {
+      url: `${authUrl}/Product/addNewProduct`,
+      method: "POST",
+      data: {},
     },
 
-    category: {
-        getAll:{
-            url: `${authUrl}/Category`,
-            method: "GET",
-        }
+    // EDIT PRODUCT
+    edit: (productId: number) => ({
+      url: `${authUrl}/Product/updateProductById/${productId}`,
+      method: "PUT",
+      data: {}, // This will be populated with the product data to update
+    }),
+
+    // DELETE PRODUCT
+    delete: (productId: number) => ({
+      url: `${authUrl}/Product/deleteProductById/${productId}`,
+      method: "DELETE",
+    }),
+  },
+
+  goodsIssue: {
+    //GET ALL
+    getAll: {
+      url: `${authUrl}/GoodRecipt/GetAllGoodRecipts`,
+      method: "GET",
+    },
+    //GET BY ID
+    getById: {
+      url: `${authUrl}/GoodRecipt`,
+      method: "GET",
     },
 
-    user: {
-        //GET ALL
-        getAll: {
-            url: `${authUrl}/User`,
-            method: "GET",
-        },
-
-        //POST FOR LOGIN
-        login: {
-            url: `${authUrl}/User/login`,
-            method: "POST",
-            data: {},
-        },
-
+    //POST
+    createNew: {
+      url: `${authUrl}/GoodRecipt/addGoodRecipt`,
+      method: "POST",
+      data: {},
     },
 
-    partner:{
-        getAll:{
-            url: `${authUrl}/Partner`,
-            method: "GET",
-        }
+    ////////////////////// ELSE //////////////////////
+    edit: {
+      url: `${authUrl}api/Product`,
+      method: "PUT",
+      token: "",
+      data: {},
+    },
+  },
+
+  category: {
+    getAll: {
+      url: `${authUrl}/Category`,
+      method: "GET",
+    },
+    edit: (id: number) => ({
+      url: `${authUrl}/Category/updateCategoryById/${id}`,
+      method: "PUT",
+      data: {},
+    }),
+    create: {
+      url: `${authUrl}/Category/addNewCategory`,
+      method: "POST",
+      data: {},
+    },
+    search: (name: string) => ({
+      url: `${authUrl}/Category/getCategoryBySearchName`,
+      method: "GET",
+      data: { name },
+    }),
+  },
+
+  user: {
+    //GET ALL
+    getAll: {
+      url: `${authUrl}/User`,
+      method: "GET",
     },
 
-    warehouse:{
-        getAll:{
-            url: `${authUrl}/WareHouse`,
-            method: "GET",
-        }
+    //POST FOR LOGIN
+    login: {
+      url: `${authUrl}/User/login`,
+      method: "POST",
+      data: {},
     },
-}
+  },
 
-export default api_links
+  partner: {
+    getAll: {
+      url: `${authUrl}/Partner`,
+      method: "GET",
+    },
+  },
+
+  warehouse: {
+    getAll: {
+      url: `${authUrl}/WareHouse`,
+      method: "GET",
+    },
+  },
+};
+
+export default api_links;
