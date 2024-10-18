20893d492c8a052847c0bad5e15a8edc3dc346bd
Tuan Nguyen <qtngmin@gmail.com>
2024-10-18 09:34:08 +0700

    Add image prop to Product and ProductModel

--- Back-End/Migrations/20241017234219_Add_image_to_product.Designer.cs
+++ Back-End/Migrations/20241017234219_Add_image_to_product.Designer.cs
@@ -4,6 +4,7 @@ using APIBackEnd.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace APIBackEnd.Migrations
 {
     [DbContext(typeof(CoreContext))]
-    partial class CoreContextModelSnapshot : ModelSnapshot
+    [Migration("20241017234219_Add_image_to_product")]
+    partial class Add_image_to_product
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Back-End/Migrations/20241017234219_Add_image_to_product.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APIBackEnd.Migrations
+{
+    public partial class Add_image_to_product : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImgPath",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImgPath",
+                table: "Products");
+        }
+    }
+}
