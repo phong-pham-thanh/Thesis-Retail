4dcb13e392c6fcb99237a9e55adeabd2ff662f60
Phong Pham Thanh <phong.pham.thanh@preciofishbone.se>
2024-11-20 19:28:31 +0700

    Fix load session

--- Front-End/angular/src/app/app.component.ts
+++ Front-End/angular/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { AuthService } from './services/auth.service';
+import { Router } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular';
+  constructor(private authService: AuthService, private router: Router, private cookieService: CookieService) {
+    this.sessionLoaded = false;
+
+  }
+  sessionLoaded = false;
+
+  ngOnInit() {
+    this.initializeSession();
+  }
+
+  initializeSession() {
+    const user = this.authService.getUserFromCookie();
+  
+    if (user && user.id) {
+      this.authService.setSession(user.id).subscribe(
+        (result) => {
+          this.cookieService.set('user', JSON.stringify(result), { path: '/' });
+          this.sessionLoaded = true;
+        },
+        (error) => {
+          console.error("Session setup failed:", error);
+          this.router.navigate(['/login']);
+          this.sessionLoaded = true;
+        }
+      );
+    } else {
+      this.router.navigate(['/login']);
+      this.sessionLoaded = true;
+    }
+  }
 }
